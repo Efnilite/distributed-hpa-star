f0bbd79e7519eb4b1fd12e620da24506e4448cd7
Efnilite <35348263+Efnilite@users.noreply.github.com>
2026-03-09 00:08:49 +0100

    Add variable

---
+++ common/vbitset.h
@@ -0,0 +1,22 @@
+// Variable-size bitset
+
+#ifndef VBITSET_H
+#define VBITSET_H
+
+// #ifdef VBITSET_IMPLEMENTATION
+
+#define _bit_header(bitset)
+
+#define bit_get_value(bitset, map, index) \
+do { \
+    const uint32_t page_idx = index >> 5;\
+    const uint8_t in_page = 31 - index % 32;\
+    const unsigned int v = map->coordinates[page_idx].v;\
+\
+    return (v & 1 << in_page) >> in_page; \
+} while (0);
+
+#define bit_free(bitset)
+
+// #endif
+#endif
