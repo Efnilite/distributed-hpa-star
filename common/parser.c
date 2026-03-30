16cd50d8e97b4b2224213336a77732f817871131
Efnilite <35348263+Efnilite@users.noreply.github.com>
2026-03-30 15:49:33 +0200

    Parser changes

--- common/parser.c
+++ common/parser.c
@@ -45,33 +45,46 @@ Map parse_map(const char* file_name)
     {
         const char cluster_c = buff[x];
         int16_t cluster_w = 1;
-        const int16_t cluster_h = 1;
+        int16_t cluster_h = 1;
 
-        char loop_c;
-        while ((loop_c = buff[x + cluster_w]) == cluster_c)
+        while (true)
         {
+            bool fail = false;
+            for (size_t dy = 0; dy < cluster_h; dy++)
+            {
+                if (buff[(x + cluster_w) + (y + dy) * w] != cluster_c)
+                {
+                    fail = true;
+                    break;
+                }
+            }
+            for (size_t dx = 0; dx < cluster_w; dx++)
+            {
+                if (buff[(x + dx) + (y + cluster_h) * w] != cluster_c)
+                {
+                    fail = true;
+                    break;
+                }
+            }
+
+            if (fail)
+            {
+                break;
+            }
+
             cluster_w++;
+            cluster_h++;
         }
 
-        block_map_add(map, (BlockMapCluster){
+        block_map_add(map,
+                      (BlockMapCluster){
                           .pos = (Vec2){x, y},
                           .dimensions = (Vec2){cluster_w, cluster_h},
                           .value = cluster_c == '@',
                       });
-
-        if (loop_c == '\n')
-        {
-            (void)fgets(buff, LINE_LENGTH, file);
-            y++;
-            x = 0;
-            continue;
-        }
-        x += cluster_w;
     }
 
     fclose(file);
 
-    return (Map){
-        .w = w, .h = h, .size = w * h, .coordinates = map
-    };
+    return (Map){.w = w, .h = h, .size = w * h, .coordinates = map};
 }
